f1ba740c10db1f9e6018e357bb0506093a307830
SpartanG318 <j.symanowski@web.de>
2022-11-21 14:05:38 +0100

    add first components

--- libs/frontend/views/src/index.ts
+++ libs/frontend/views/src/index.ts
@@ -1,3 +1,7 @@
 export * from './lib/frontend-views.module';
 
 export * from './lib/home/home.component';
+
+export * from './lib/top-menu/top-menu.component';
+
+export * from './lib/sprint/sprint.component';
--- libs/frontend/views/src/lib/frontend-views.module.ts
+++ libs/frontend/views/src/lib/frontend-views.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { SprintComponent } from './sprint/sprint.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HomeComponent],
-  exports: [HomeComponent],
+  declarations: [HomeComponent, TopMenuComponent, SprintComponent],
+  exports: [HomeComponent, TopMenuComponent, SprintComponent],
 })
 export class FrontendViewsModule {}
