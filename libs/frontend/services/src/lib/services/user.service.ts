fd436098fac267c7fb5b9205fda82d0fdc28915f
SpartanG318 <j.symanowski@web.de>
2022-12-01 08:12:13 +0100

    outcommented non functional mat table

--- libs/frontend/services/src/lib/services/user.service.ts
+++ libs/frontend/services/src/lib/services/user.service.ts
@@ -1,16 +1,15 @@
 /* eslint-disable @nrwl/nx/enforce-module-boundaries */
 import { HttpClient } from '@angular/common/http';
-import { User } from '@athena/frontend/views';
-import { map } from 'rxjs/operators';
 
 import { environment } from './environment';
 
 export class UserService {
-  constructor(private httpClient: HttpClient) {}
+  // users: User[];
+  constructor(private httpClient: HttpClient) {
+    // this.users = this.getAllUser();
+  }
   host = environment.athenaHost;
   getAllUser() {
-    return this.httpClient
-      .get(this.host + '/user/userList')
-      .pipe(map((users) => users as User[]));
+    return this.httpClient.get(this.host + '/user/userList');
   }
 }
